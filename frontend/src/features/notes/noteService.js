12b6400383b6f15d07cac2decaafabb06a475f6a
Grzegorz Ryczkowski <ryczkowski.grzegorz@gmail.com>
2023-03-15 22:35:02 +0100

    Adding note functionality to backend and frontend

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+const createNote = async (noteText, ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.post(
+    API_URL + ticketId + '/notes',
+    { text: noteText },
+    config
+  );
+
+  return response.data;
+};
+
+const getNotes = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(API_URL + ticketId + '/notes', config);
+
+  console.log(response);
+  return response.data;
+};
+
+const noteService = {
+  getNotes,
+  createNote,
+};
+
+export default noteService;
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import BackButton from '../components/BackButton';
+import NoteItem from '../components/NoteItem';
 import Spinner from '../components/Spinner';
+import { createNote, getNotes } from '../features/notes/noteSlice';
 import { closeTicket, getTicket } from '../features/tickets/ticketSlice';
+import Modal from 'react-modal';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
 
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
+  );
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
   );
 
   const navigate = useNavigate();
@@ -21,11 +46,12 @@ function Ticket() {
       toast.error(message);
     }
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
 
     //eslint-disable-next-line
   }, [isError, message, ticketId]);
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -35,6 +61,20 @@ function Ticket() {
     dispatch(closeTicket(ticketId));
     toast.success('Ticket closed');
     navigate('/tickets');
+  }
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    dispatch(createNote({ noteText, ticketId }));
+    closeModal();
   }
 
   return (
@@ -56,9 +96,49 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
 
-      {ticket.status != 'closed' && (
+      {ticket.status !== 'closed' && (
+        <button onClick={openModal} className="btn">
+          <AiOutlinePlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Note"
+      >
+        <h2>Add Note</h2>
+        <button onClick={closeModal} className="btn-close">
+          X
+        </button>
+        <form onSubmit={onFormSubmit}>
+          <div className="form-group">
+            <textarea
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="Type a note..."
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== 'closed' && (
         <button onClick={onTicketClose} className="btn btn-block btn-danger">
           Close Ticket
         </button>
